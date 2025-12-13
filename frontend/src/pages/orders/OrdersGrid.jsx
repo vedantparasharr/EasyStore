9be2278f217bae7ea1a151f5d84aa217ef7c8323
Vedant Parashar <111298553+vedantparasharr@users.noreply.github.com>
2025-12-13 12:43:28 +0530

    Fix order time formatting to ensure number conversion

--- frontend/src/pages/orders/OrdersGrid.jsx
+++ frontend/src/pages/orders/OrdersGrid.jsx
@@ -26,7 +26,7 @@ export function OrdersGrid({ loadCart }) {
                     <div className="order-header-left-section">
                         <div className="order-date">
                             <div className="order-header-label">Order Placed:</div>
-                            <div>{dayjs(order.orderTimeMs).format('MMMM DD')}</div>
+                            <div>{dayjs(Number(order.orderTimeMs)).format("MMMM DD")}</div>
                         </div>
                         <div className="order-total">
                             <div className="order-header-label">Total:</div>
